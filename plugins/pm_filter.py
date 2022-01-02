46008fc7a4b62dc983de74044a07599cfc186564
Alifmuhammed <95561254+Alifmuhammed@users.noreply.github.com>
2022-01-02 14:38:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -27,7 +27,7 @@ BUTTONS = {}
 SPELL_CHECK = {}
 
 
-    )
+
 @Client.on_message(filters.regex("http") | filters.regex("t.me") | filters.regex("youtu.be") | filters.regex("com") | filters.regex("https") | filters.regex("/" ) | filters.regex("myre") | filters.regex("patti") | filters.service)
 async def delete(bot,message):
  await message.delete()
